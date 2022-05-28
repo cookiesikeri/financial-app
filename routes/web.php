d1970e677d713801d7d041d5d1835e1b058a3a34
cookiesikeri <ikbenezer@gmail.com>
2022-05-29 00:36:04 +0100

    34

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return [
-        'test' => "Welcome to Transave Nigeria System Api Live APP..."
+        'test' => "Welcome to Transave Nigeria System Api Live APP......"
     ];
 });
 
